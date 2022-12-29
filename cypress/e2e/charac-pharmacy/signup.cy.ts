a0ef409ab16ba239a5ee10fd4af7e73752b40e0e
beemi <beemi.raja@gmail.com>
2022-12-29 11:18:20 +0000

    feat: added intercepts

--- cypress/e2e/charac-pharmacy/signup.cy.ts
+++ cypress/e2e/charac-pharmacy/signup.cy.ts
@@ -1,6 +1,11 @@
 describe("charac pharmacy signup", () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
+        // Network calls
+        cy.intercept({
+            method: "POST",
+            url: "**/auth/signup",
+        }).as("signupApi");
 
         cy.visit("/#/home");
     });
@@ -13,5 +18,9 @@ describe("charac pharmacy signup", () => {
         cy.xpath("//ion-text[normalize-space()='I am a Customer']").should("be.visible").click().log("clicked on I am a customer button");
 
         cy.signUp("test","test","077721234566","test121212@example.com","test1234");
+
+        cy.wait("@signupApi").then((interception) => {
+            expect(interception.response.statusCode, "Signup auth API failed").to.eq(200);
+        })
     });
 });
